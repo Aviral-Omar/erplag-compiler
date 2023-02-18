2df48e42b8fa71b60ffc7c3b2bd0daa2a48015e9
Aviral-Omar <aviralomar0301@gmail.com>
2023-02-19 00:03:39 +0530

    initial file setup

---
+++ code/driver.c
@@ -0,0 +1,9 @@
+// Take the input file name and buffer size at command line.
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
+
+/* TODO DELETE
+https://www.geeksforgeeks.org/const-qualifier-in-c/
+ */
