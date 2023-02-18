a4892d26de6ef9bc6c4ba44fdef5ae8c02938166
Aviral-Omar <aviralomar0301@gmail.com>
2023-02-19 01:34:58 +0530

    checked argument validity

--- code/driver.c
+++ code/driver.c
@@ -1,9 +1,34 @@
-// Take the input file name and buffer size at command line.
-int main(int argc, char const *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+
+// Take the input file name and buffer size at command line
+int main(int argc, char *argv[])
 {
+	// Checks if program receives 2 arguments from command line
+	if (argc != 3) {
+		printf("Please enter the filename and buffer size as arguments respectively.");
+		exit(EXIT_FAILURE);
+		/* TODO Delete
+		Run "man 3 exit"
+		*/
+	}
+
+	char *fileName = argv[1];
+	FILE *fp = fopen(fileName, "r");
+	// Checks if file path is valid
+	if (!fp) {
+		printf("File does not exist or unable to access file.");
+		exit(EXIT_FAILURE);
+	}
+
+	char *endPtr;
+	int bufferSize = (int)strtol(argv[2], &endPtr, 10);
+	// Checks if buffer size is valid
+	if (!(*argv[2] != '\0' && *endPtr == '\0')) {
+		printf("Invalid buffer size.");
+		exit(EXIT_FAILURE);
+	}
+
+
 	return 0;
 }
-
-/* TODO DELETE
-https://www.geeksforgeeks.org/const-qualifier-in-c/
- */
