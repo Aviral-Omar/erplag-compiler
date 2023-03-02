bde390232e9c312d8d24e25fa42eb27d25de3073
Aviral-Omar <aviralomar0301@gmail.com>
2023-03-02 13:53:14 +0530

    added group details in comments

--- code/lookupTable.c
+++ code/lookupTable.c
@@ -1,3 +1,9 @@
+/* GROUP 51
+Arnav Agarwal:			2019B2A70966P
+Aviral Omar:			2019B3A70411P
+Chandra Sekhar Reddy E:	2019B4A70634P
+Vatsal Pattani:			2019B5A70697P
+*/
 #include <stdlib.h>
 #include <string.h>
 
